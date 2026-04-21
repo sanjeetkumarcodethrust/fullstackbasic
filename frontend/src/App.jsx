274734e77705515c93c416d65d14d7c22ca73b21
Sanjeet kumar <sanjeetkum960@gmail.com>
2026-04-21 18:30:23 +0530

    change in the code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,8 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import axios from 'axios'
-
 function App() {
   const [jokes, setJokes] = useState([])
-
-
   useEffect(() => {    axios.get('/api/jokes')
       .then(response => {
         setJokes(response.data)
@@ -17,22 +14,17 @@ function App() {
         console.error('Error fetching jokes:', error)
       })
   }, [])
-
   return (
    <>
    <h1>Chai and Full Stack</h1>
    <p>JOKES :{jokes.length}</p>
-
     {jokes.map((joke, index) => (
       <div key={joke.id}>
         <h3>{joke.title}</h3>
         <p>{joke.joke}</p>
       </div>
     ))}
-
-
    </>
   )
 }
-
 export default App
